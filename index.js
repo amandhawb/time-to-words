28bc020721d0b75d6433782fb15fe56f8879226c
Amandha W. Barok <amandha.barok@gmail.com>
2024-03-17 21:12:54 -0400

    Amandha's changes

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
-function convertTimeToWords(time) {
-  // TODO: real code goes here!
-  if (time === '0:00') {
-    return 'midnight';
-  }
+const hoursInString = {
+  1 : 'one',
+  2 : 'two',
+  3 : 'three', 
+  4 : 'four',
+  5 : 'five',
+  6 : 'six',
+  7 : 'seven',
+  8 : 'eight',
+  9 : 'nine', 
+  10 : 'ten',
+  11 : 'eleven',
+  12 : 'twelve'
+};
 
-  return 'half past eight';
+function handleQuarterTimes(hours, minutes) {
+  if(minutes === 45) {
+    let hoursToBeReturned = hours + 1;
+    return 'quarter to ' + hoursInString[hoursToBeReturned];
+  } else if(minutes === 15) {
+    return 'quarter past ' + hoursInString[hours];
+  }
+}
+
+function handleOClock(hours, minutes) {
+  return hoursInString[hours] + " o'clock"
+}
+
+function convertTimeToWords(time) {
+  const splittedTime = time.split(':');
+  let hours = parseInt(splittedTime[0]);
+  let minutes = parseInt(splittedTime[1]);
+
+  if (hours === 0 && minutes === 0) {
+    return 'midnight';
+  } else if(hours === 12 && minutes === 0) {
+    return 'midday';
+  } else if(minutes === 15 || minutes === 45) {
+    return handleQuarterTimes(hours, minutes)
+  } else if(minutes === 0) {
+    return handleOClock(hours, minutes)
+  }
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -6,13 +6,29 @@ describe('Time to words', () => {
     expect(timeInWords).toBe('midnight');
   });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
-  });
+  // it('Handles 30 - 8:30', () => {
+  //   const timeInWords = convertTimeToWords('8:30');
+  //   expect(timeInWords).toBe('half past eight');
+  // });
 
-  it('Handles times after 30 mins - 2:45', () => {
+  it('Handles times with 45 mins - 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
+  it('Handles times with 15 mins - 2:15', () => {
+    const timeInWords = convertTimeToWords('2:15');
+    expect(timeInWords).toBe('quarter past two');
+  });
+
+  it('Handles midday', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it("Handles o'clock", () => {
+    const timeInWords = convertTimeToWords('3:00');
+    expect(timeInWords).toBe("three o'clock");
+  });
+  
 });
