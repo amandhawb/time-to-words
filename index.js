c0f490caf48a67a6d7e8525835444a5abc2e8639
Amandha W. Barok <amandha.barok@gmail.com>
2024-03-17 23:08:07 -0400

    Adding final changes

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-// expecting time to be a string in the format like '8:15' or '12:30'
 const hoursInString = {
-  1 : 'one',
-  2 : 'two',
-  3 : 'three', 
-  4 : 'four',
-  5 : 'five',
-  6 : 'six',
-  7 : 'seven',
-  8 : 'eight',
-  9 : 'nine', 
-  10 : 'ten',
-  11 : 'eleven',
-  12 : 'twelve'
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+  10: 'ten',
+  11: 'eleven',
+  12: 'twelve',
+  13: 'thirteen',
+  14: 'fourteen',
+  15: 'quarter',
+  16: 'sixteen',
+  17: 'seventeen',
+  18: 'eighteen',
+  19: 'nineteen',
+  20: 'twenty',
+  21: 'twenty one',
+  22: 'twenty two',
+  23: 'twenty three',
+  24: 'twenty four',
+  25: 'twenty five',
+  26: 'twenty six',
+  27: 'twenty seven',
+  28: 'twenty eight',
+  29: 'twenty nine',
+  30: 'half'
 };
 
-function handleQuarterTimes(hours, minutes) {
-  if(minutes === 45) {
+function handleGenericRule(hours, minutes) {
+  if(minutes > 30) {
     let hoursToBeReturned = hours + 1;
-    return 'quarter to ' + hoursInString[hoursToBeReturned];
-  } else if(minutes === 15) {
-    return 'quarter past ' + hoursInString[hours];
+    let minutesRemaining = 60 - minutes;
+    return hoursInString[minutesRemaining] +' to ' + hoursInString[hoursToBeReturned];
+  } else {
+    return hoursInString[minutes] + ' past ' + hoursInString[hours];
   }
 }
 
-function handleOClock(hours, minutes) {
+function handleOClock(hours) {
   return hoursInString[hours] + " o'clock"
 }
 
@@ -36,11 +54,11 @@ function convertTimeToWords(time) {
     return 'midnight';
   } else if(hours === 12 && minutes === 0) {
     return 'midday';
-  } else if(minutes === 15 || minutes === 45) {
-    return handleQuarterTimes(hours, minutes)
   } else if(minutes === 0) {
-    return handleOClock(hours, minutes)
-  }
+    return handleOClock(hours)
+  } else {
+    return handleGenericRule(hours, minutes)
+  } 
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -5,12 +5,7 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('0:00');
     expect(timeInWords).toBe('midnight');
   });
-
-  // it('Handles 30 - 8:30', () => {
-  //   const timeInWords = convertTimeToWords('8:30');
-  //   expect(timeInWords).toBe('half past eight');
-  // });
-
+  
   it('Handles times with 45 mins - 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
@@ -29,6 +24,19 @@ describe('Time to words', () => {
   it("Handles o'clock", () => {
     const timeInWords = convertTimeToWords('3:00');
     expect(timeInWords).toBe("three o'clock");
+  });
+
+  it("Handles number between 1 minutes to 30 minutes", () => {
+    expect(convertTimeToWords('2:03')).toBe("three past two");
+    expect(convertTimeToWords('2:11')).toBe("eleven past two");
+    expect(convertTimeToWords('2:15')).toBe("quarter past two");
+    expect(convertTimeToWords('2:30')).toBe("half past two");
+  });
+
+  it("Handles number between 30 minutes to 60 minutes", () => {
+    expect(convertTimeToWords('2:33')).toBe("twenty seven to three");
+    expect(convertTimeToWords('2:40')).toBe("twenty to three");
+    expect(convertTimeToWords('2:55')).toBe("five to three");
   });
   
 });
